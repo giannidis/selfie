7a21305a15439006fd4927bdd021d679a7618ee2
kostaskiou <kostaskiou@hotmail.gr>
2015-06-01 13:38:31 +0300

    fix

--- app/controllers/selfies_controller.rb
+++ app/controllers/selfies_controller.rb
@@ -17,7 +17,7 @@ def create
       :payment_method_nonce => params[:payment_method_nonce]
     )
     if result.success?
-      notice: "Payment was successful"
+      flash[:notice] = "Payment was successful"
     else
       flash[:notice] = "Payment unsuccessful"
     end
